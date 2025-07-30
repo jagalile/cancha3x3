a6e68d000d7ade40b06120b5bdc96eea0700b361
Javier Galilea <javier.galilea.pesos@gmail.com>
2025-07-30 09:55:10 +0200

    Add landing page template, update URL routing, and configure static files; enhance base template and footer structure for improved user experience.

--- config/settings/local.py
+++ config/settings/local.py
@@ -4,4 +4,8 @@ from.base import *
 # En desarrollo, permitimos todas las cabeceras
 ALLOWED_HOSTS = ['*']
 
+STATICFILES_DIRS = [
+    BASE_DIR / "src/static",
+]
+
 # Configuraciones específicas para desarrollo, como django-debug-toolbar
